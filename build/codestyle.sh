f0e91bfa81ac974619bb82c3bb971bec89ff8623
Maarten Kuiper <maarten.kuiper@deinternetjongens.nl>
2018-09-07 14:10:07 +0200

    Build should fail when phpcs or php linting fails

--- build/codestyle.sh
+++ build/codestyle.sh
@@ -5,6 +5,16 @@ if [[ "$osName" == 'Darwin' ]]; then
     vendor/bin/phpcbf --standard=CS-DIJ.xml ./config ./src ./tests
 fi
 
-vendor/bin/phpcs --standard=CS-DIJ.xml ./config ./src ./tests
+array=(
+    "vendor/bin/phpcs --standard=CS-DIJ.xml ./config ./src ./tests"
+    "find -L ./config ./src ./tests -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l"
+)
 
-find -L ./config ./src ./tests -name '*.php' -print0 | xargs -0 -n 1 -P 4 php -l
+for ix in ${!array[*]}
+do
+    eval "${array[$ix]}"
+    if [ "$?" -ne "0" ]; then
+        echo "Command failed"
+        exit 1
+    fi
+done
