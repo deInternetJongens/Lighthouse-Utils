35438722760c1c9a80c6b98792e8dfb01a4b0250
Maarten Kuiper <maarten.kuiper@deinternetjongens.nl>
2018-09-07 12:34:59 +0200

    Add PostalCodeNL support for input fields generator

--- src/Generators/Arguments/InputFieldsArgumentGenerator.php
+++ src/Generators/Arguments/InputFieldsArgumentGenerator.php
@@ -4,6 +4,7 @@ namespace DeInternetJongens\LighthouseUtils\Generators\Arguments;
 
 use DeInternetJongens\LighthouseUtils\Schema\Scalars\Date;
 use DeInternetJongens\LighthouseUtils\Schema\Scalars\DateTimeTz;
+use DeInternetJongens\LighthouseUtils\Schema\Scalars\PostalCodeNl;
 use GraphQL\Type\Definition\EnumType;
 use GraphQL\Type\Definition\FloatType;
 use GraphQL\Type\Definition\IntType;
@@ -21,6 +22,7 @@ class InputFieldsArgumentGenerator
         Date::class,
         DateTime::class,
         DateTimeTz::class,
+        PostalCodeNl::class,
         EnumType::class
     ];
 
