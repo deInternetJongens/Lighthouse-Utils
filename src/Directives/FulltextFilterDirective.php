f98b1d6a27453ef2bf9f7f4dd0ad924e96c7435f
Tim Mourik <tim@deinternetjongens.nl>
2018-09-27 13:27:30 +0200

    FulltextFilterDirective

---
+++ src/Directives/FulltextFilterDirective.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DeInternetJongens\LighthouseUtils\Directives;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class FulltextFilterDirective extends BaseDirective
+{
+
+    public function name(): string
+    {
+        return 'fulltext';
+    }
+
+    public function handle(string $fieldName, string $value, Builder $builder): Builder
+    {
+       return $builder->getModel()->fullTextSearch($builder, $value);
+    }
+}
