08ec585e26f114de1da3da1fbcb40544c110e61b
Maarten Kuiper <maarten.kuiper@deinternetjongens.nl>
2018-10-18 14:26:26 +0200

    Generate schema using new event instead of hacking around with config

--- src/Generators/SchemaGenerator.php
+++ src/Generators/SchemaGenerator.php
@@ -27,6 +27,7 @@ use GraphQL\Type\Definition\ObjectType;
 use GraphQL\Type\Definition\StringType;
 use GraphQL\Type\Definition\Type;
 use GraphQL\Type\Schema;
+use Nuwave\Lighthouse\Events\BuildingAST;
 use Nuwave\Lighthouse\Schema\Source\SchemaSourceProvider;
 use Nuwave\Lighthouse\Schema\Types\Scalars\DateTime;
 
@@ -162,97 +163,39 @@ class SchemaGenerator
      */
     private function getSchemaForFiles(array $definitionFileDirectories): Schema
     {
-        $originalSchemaFilePath = Config::get('lighthouse.schema.register');
+        resolve('events')->listen(
+            BuildingAST::class,
+            function () use ($definitionFileDirectories) {
+                $typeDefinitionPaths = $this->definitionsParser->getGraphqlDefinitionFilePaths(
+                    $definitionFileDirectories['types']
+                );
+                $relativeTypeImports = $this->concatSchemaDefinitionFilesFromPath($typeDefinitionPaths);
 
-        //Get a temp folder and file
-        $schemaDirectory = dirname(config('lighthouse.schema.register'));
-        $tempSchemaFilePath = $schemaDirectory . '/tempschema.graphql';
-
-        $typeDefinitionPaths = $this->definitionsParser->getGraphqlDefinitionFilePaths(
-            $definitionFileDirectories['types']
+                // Webonyx GraphQL will not generate a schema if there is not at least one query
+                // So just pretend we have one
+                $placeholderQuery = 'type Query{placeholder:String}';
+                return "$relativeTypeImports\r\n$placeholderQuery";
+            }
         );
-        $relativeTypeImports = $this->generateGraphqlRelativeImports($typeDefinitionPaths);
 
-        if (! file_exists($schemaDirectory)) {
-            mkdir($schemaDirectory, 0777, true);
-        }
-
-        // Webonyx GraphQL will not generate a schema if there is not at least one query
-        // So just pretend we have one
-        $placeholderQuery = 'type Query{placeholder:String}';
-        $tempSchemaFile = fopen($tempSchemaFilePath, 'wb');
-        fwrite($tempSchemaFile, sprintf("%s\r\n%s", $relativeTypeImports, $placeholderQuery));
-
-        //Override the config value where Lighthouse parses it's schema from
-        $this->schemaSourceProvider->setRootPath($tempSchemaFilePath);
         $schema = graphql()->prepSchema();
-
-        fclose($tempSchemaFile);
-        unlink($tempSchemaFilePath);
-
-        //Set the config value back to where we want to the original path
-        $this->schemaSourceProvider->setRootPath($originalSchemaFilePath);
 
         return $schema;
     }
 
     /**
-     * Generates
-     *
      * @param array $schemaDefinitionFilePaths
      * @return string
      */
-    private function generateGraphqlRelativeImports(array $schemaDefinitionFilePaths): string
+    private function concatSchemaDefinitionFilesFromPath(array $schemaDefinitionFilePaths): string
     {
-        $imports = [];
-        foreach ($schemaDefinitionFilePaths as $file) {
-            $file = $this->getRelativePath(dirname(config('lighthouse.schema.register')), $file);
-            $imports[] = sprintf('#import %s', $file);
+        $concatenatedImports = '';
+        foreach ($schemaDefinitionFilePaths as $filePath) {
+            $concatenatedImports .= file_get_contents($filePath);
+            $concatenatedImports .= "\r\n";
         }
 
-        return implode("\r\n", $imports);
-    }
-
-    /**
-     * Find the relative file system path between two file system paths
-     * As stolen from: https://gist.github.com/ohaal/2936041
-     *
-     * @param  string $frompath Path to start from
-     * @param  string $topath Path we want to end up in
-     * @return string             Path leading from $frompath to $topath
-     */
-    private function getRelativePath($frompath, $topath)
-    {
-        $from = explode(DIRECTORY_SEPARATOR, $frompath); // Folders/File
-        $to = explode(DIRECTORY_SEPARATOR, $topath); // Folders/File
-        $relpath = '';
-
-        $i = 0;
-        // Find how far the path is the same
-        while (isset($from[$i]) && isset($to[$i])) {
-            if ($from[$i] != $to[$i]) {
-                break;
-            }
-            $i++;
-        }
-        $j = count($from) - 1;
-        // Add '..' until the path is the same
-        while ($i <= $j) {
-            if (! empty($from[$j])) {
-                $relpath .= '..' . DIRECTORY_SEPARATOR;
-            }
-            $j--;
-        }
-        // Go to folder from where it starts differing
-        while (isset($to[$i])) {
-            if (! empty($to[$i])) {
-                $relpath .= $to[$i] . DIRECTORY_SEPARATOR;
-            }
-            $i++;
-        }
-
-        // Strip last separator
-        return substr($relpath, 0, -1);
+        return $concatenatedImports;
     }
 
     /**
